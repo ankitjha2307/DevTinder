00ecf374a5db29fd74d970842c065eb8bff37417
ankit <ankitjha0723@gmail.com>
2025-07-27 03:25:30 +0530

    Install Postman

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.use("/hello", (req, res) => {
+  res.send("Hello Hello Hello");
+});
+
+app.use("/test", (req, res) => {
+  res.send("testing the server");
+});
+
 app.use((req, res) => {
   res.send("Hello from the server! yes");
 });
