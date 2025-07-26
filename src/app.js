9b2fd8b13b9b092bcf22d559e076b7a3c972ccd3
ankit <ankitjha0723@gmail.com>
2025-07-27 01:39:46 +0530

    make Server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res) => {
+  res.send("Hello from the server! yes");
+});
+
+app.listen(3000, () => {
+  console.log("server is succefully listing on 3000....");
+});
