05dbb0b04e0d089c788e3cf6804ad61fc3d29f5b
ankit <ankitjha0723@gmail.com>
2025-07-31 21:11:10 +0530

    ignore/accept connection request

--- src/routes/reques.js
+++ src/routes/reques.js
@@ -1,14 +1,62 @@
 const express = require("express");
 const { userAuth } = require("../Middleware/auth");
-
+const ConnectionRequest = require("../model/connectionRequest");
 const requestRouter = express.Router();
+const User = require("../model/user");
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
 
-  console.log("Connection request sending...");
+      const allowedStatus = ["ignored", "intrested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type " + status });
+      }
 
-  res.send(user.firstName + " send the connection request");
-});
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(400).json({
+          message: "User Not Found",
+        });
+      }
+
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection request Alredy Exists " });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message:
+          req.user.firstName + "  is " + status + " in " + toUser.firstName,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
+  }
+);
 
 module.exports = requestRouter;
