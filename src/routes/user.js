1d5ea2ad50d4117a8e485a59f470cb3fdc67a32e
ankit <ankitjha0723@gmail.com>
2025-08-01 15:36:16 +0530

    Connection request

---
+++ src/routes/user.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const userRouter = express.Router();
+const { userAuth } = require("../Middleware/auth");
+const ConnectionRequest = require("../model/connectionRequest");
+
+userRouter.get("/user/request/recived", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "intrested",
+    });
+
+    res.json({ message: "Data fetched Succesfully", data: connectionRequest });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+module.exports = userRouter;
